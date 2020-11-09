cd30dac4f63ca49b08821a11760d652add9b6ee6
farnazdargahi <57144153+farnazdargahi@users.noreply.github.com>
2020-11-08 21:45:28 -0800

    Update test_pywebapp.py

--- test_pywebapp.py
+++ test_pywebapp.py
@@ -6,7 +6,7 @@ b = app.browser()
 
 def test_Default():
     b.open('/')
-    assert 'COMP4964' in b.get_text()
+    assert 'A00123456' in b.get_text()
 
 def test_Api():
     b.open('/api/v1/test')
