da42a3b1020460c97732980bf001bac296bb8cb3
farnazdargahi <57144153+farnazdargahi@users.noreply.github.com>
2020-11-10 09:23:26 -0800

    Update pywebapp.py

--- pywebapp.py
+++ pywebapp.py
@@ -13,7 +13,7 @@ class Default:
         hostname = socket.gethostname()
         return """<html>
         <title>PyWebApp</title>
-        <body bgcolor="gray">
+        <body bgcolor="light gray">
         <h1>Hello COMP4964!</h1>
         <p>Available API methods:</p>
         <ul>
