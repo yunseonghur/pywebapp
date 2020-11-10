cb44e6567608312f68352cd88ed539ffb289a88b
yunseonghur <yshuh0126@gmail.com>
2020-11-10 12:11:37 -0800

    Update test_pywebapp.py

--- test_pywebapp.py
+++ test_pywebapp.py
@@ -6,7 +6,7 @@ b = app.browser()
 
 def test_Default():
     b.open('/')
-    assert 'COMP4964' in b.get_text()
+    assert 'A01063658' in b.get_text()
 
 def test_Api():
     b.open('/api/v1/test')
